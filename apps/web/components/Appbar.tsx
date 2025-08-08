163cb982d1ad60a2866a370887faecbe7058d038
OM <omjaiswal2552@gmail.com>
2025-08-08 22:43:43 +0530

    8aug

--- apps/web/components/Appbar.tsx
+++ apps/web/components/Appbar.tsx
@@ -2,7 +2,16 @@
 
 export const Appbar = () => {
 
-    return <>
-        
-    </>
+    return <div className="flex justify-between">
+        <h1 className="pl-4 font-bold text-red-500">devConnect</h1>
+        <div className="flex justify-between space-x-4">
+            <p>Home</p>
+            <p>Invitations</p>
+        </div>
+        <div className="flex pr-4 justify-between space-x-4">
+            <p>Join Now</p>
+            <p>Signin</p>
+        </div>
+
+    </div>
 }

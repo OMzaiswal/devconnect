8a179101a531214c1334101584484fb5e67a1e73
OM <omjaiswal2552@gmail.com>
2025-08-11 23:48:42 +0530

    added shadcn in web

--- apps/web/components/Appbar.tsx
+++ apps/web/components/Appbar.tsx
@@ -2,11 +2,13 @@
 
 export const Appbar = () => {
 
-    return <div className="flex justify-between">
-        <h1 className="pl-4 font-bold text-red-500">devConnect</h1>
+    return <div className="flex justify-between py-5 text-2xl bg-blue-500 text-white">
+        <h1 className="pl-4 font-bold  text-4xl">devConnect</h1>
         <div className="flex justify-between space-x-4">
             <p>Home</p>
             <p>Invitations</p>
+            <p>Messages</p>
+            <p>Notifications</p>
         </div>
         <div className="flex pr-4 justify-between space-x-4">
             <p>Join Now</p>

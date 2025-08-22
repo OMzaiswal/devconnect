ad2cce6aecd09ea5501c7f012e8cc40f83b56cca
OM <omjaiswal2552@gmail.com>
2025-08-22 08:56:38 +0530

    modified firebase.ts

--- apps/web/lib/firebase.ts
+++ apps/web/lib/firebase.ts
@@ -9,13 +9,12 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAroUbDwPqpGRLrCxjgR4DMjllYdxwnHpk",
-  authDomain: "devconnnect-om.firebaseapp.com",
-  projectId: "devconnnect-om",
-  storageBucket: "devconnnect-om.firebasestorage.app",
-  messagingSenderId: "999226732130",
-  appId: "1:999226732130:web:166b3af87387f07bd56b3b",
-//   measurementId: "G-SPWL85P54M"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
